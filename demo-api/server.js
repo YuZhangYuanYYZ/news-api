e66d60bc0bd5756597c15202c767d58e56e621a9
Jing Liu <liujing92684@163.com>
2019-04-29 08:51:19 +1000

    add a demo server

---
+++ demo-api/server.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+
+
+let db;
+const url = 'mongodb://localhost:27017/';
+const mongoClient = new MongoClient(url);
+mongoClient.connect().then((client) => {
+    const DB_NAME = 'quotes';
+    db = client.db(DB_NAME);
+    app.listen(process.env.PORT || 3000, () => {
+        console.log('listening on 3000')
+    })
+})
+
+app.get('/quotes', (req, res) => {
+    db.collection('quotes').find().toArray((err, result) => {
+        if (err) return console.log(err)
+        res.json({
+            quotes: result
+        })
+    })
+})
+
+app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+        console.log('saved to database')
+        res.json(result)
+    })
+})
+
+app.put('/quotes/:id', (req, res) => {
+    db.collection('quotes')
+        .findOneAndUpdate({
+            name: 'Yoda'
+        }, {
+            $set: {
+                name: req.body.name,
+                quote: req.body.quote
+            }
+        }, {
+            sort: {
+                _id: -1
+            },
+            upsert: true
+        }, (err, result) => {
+            if (err) return res.send(err)
+            res.json(result)
+        })
+})
+
+app.delete('/quotes/:id', (req, res) => {
+    db.collection('quotes').findOneAndDelete({
+        name: req.body.name
+    }, (err, result) => {
+        if (err) return res.send(500, err)
+        res.json('A darth vadar quote got deleted')
+    })
+})
