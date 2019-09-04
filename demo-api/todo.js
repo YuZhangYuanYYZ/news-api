eb399bef0106c85c69aa70a0f9c41566aba4639d
yyz <yuanyuzhangyyzz@163.com>
2019-09-04 22:31:16 +0800

    add post to the todo.js

--- demo-api/todo.js
+++ demo-api/todo.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 //对body-parser进行配置
 app.use( bodyParser.urlencoded({extended: true}) )
 app.use(bodyParser.json());
+app.use(cors({ origin: 'http://localhost:3000' , credentials : true}));
 
-app.listen(3000, () => {
-    console.log('示例应用正在监听 3000 端口!');
+app.listen(3004, () => {
+    console.log('示例应用正在监听 3004 端口!');
 });
 let todos=[
     {
@@ -37,6 +39,7 @@ app.get("/todos/:id",(req,res)=>{
 
 app.delete("/todos/:id",(req,res)=>{
     const numberId1 = Number(req.params.id);
+    
     todos=todos.filter((todo)=>todo.id!==numberId1);
     res.send({});
 })
@@ -60,5 +63,10 @@ app.put("/todos/:id",(req,res)=>{
     }
 })
 
-
-
+app.post("/todos",(req,res)=>{
+  const numberId1 = todos[todos.length-1].id+1;
+  const reqTodo = req.body;
+  newTodo = {id:numberId1,text:reqTodo.text,completed:false};
+  todos = [...todos,newTodo];
+  res.json(newTodo)
+})
